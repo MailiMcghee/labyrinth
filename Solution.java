42f01df1ed15f47c829136fe35c4b70da7da9539
MailiMcghee <89931259+MailiMcghee@users.noreply.github.com>
2021-12-07 14:10:15 -0500

    initial commit

---
+++ Solution.java
@@ -0,0 +1,39 @@
+public class Solution{
+    
+    int[] solution;
+    int UP;
+    int DOWN;
+    int LEFT;
+    int RIGH;
+    int rows;
+    int cols;
+    Labyrinth l;
+    int[][] grid;
+
+public Solution(){
+
+        l = new Labyrinth(rows, cols);
+        grid = new int[rows][cols];
+}
+
+public int[] solveLabyrinth( Labyrinth l){
+
+     for( int cr = 0; cr < grid[rows-1][cols-1]; cr++){
+         for(int cc =;)
+         
+        return solution;
+     }
+        
+ 
+
+}
+}
+
+
+
+    
+    
+
+
+
+   
